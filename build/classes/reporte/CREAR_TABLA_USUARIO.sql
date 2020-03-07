849b75893073030aec701553a8770dc5efc0789e
Cindy John <ccjohn1980mail.com>
2020-03-06 22:53:09 -0600

    first commit

---
+++ build/classes/reporte/CREAR_TABLA_USUARIO.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuario(
+    dni INT,
+    multa VARCHAR(30),
+    monto INT,
+    correo VARCHAR (30)
+);
---
+++ src/reporte/Reporte.java
@@ -0,0 +1,51 @@
+package reporte;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class Reporte {
+    public static Connection conectar (){
+        Connection con = null;
+        Statement st;
+        
+        try {
+            String url = "jdbc:mysql://localhost/tienda?user=root&password=mysqladmin";
+            con = DriverManager.getConnection(url);
+            if (con != null){
+                System.out.println("Conexion Satisfactoria");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.err.println("con.--");
+        return con;
+    }
+
+    public static void main(String[] args) {
+        try {
+            JasperReport jr = (JasperReport)
+            JRLoader.loadObject
+            (Reporte.class.getResource("./Reporte1.jasper"));
+            Map parametros = new HashMap<String, Object>();
+            parametros.put ("PARAMETRO_NOMBRE", "Hola Mundo");
+            JasperPrint jp = JasperFillManager.fillReport
+                (jr, parametros, conectar ());
+            
+            JasperViewer jv = new JasperViewer (jp);
+            jv.show ();
+            
+        } catch (Exception e) {
+            System.err.print(e);
+        }
+       
+    }
+    
+}
